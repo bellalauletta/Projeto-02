f3c559ba7558d01524ac48807a8715423a064d08
bellalauletta <laulettaisabella@gmail.com>
2026-03-21 14:58:29 -0300

    desafio de função

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 let titulo = document.querySelector("h1");
-    titulo.innerHTML = "Jogo do número secreto";
+    titulo.innerHTML = "hora do desafio";
 
-    let paragrafo = document.querySelector("p");
-    paragrafo.innerHTML = "Tente advinhar o número secreto entre 1 e 10";
-
-    function verificarChute() { 
+    function verificarBotaoChute() { 
         console.log("o botão foi clicado")
     }
+
+    function verificarBotaoAlerta() {
+        alert("Eu amo JS")
+    }
+    function exibirPrompt () {
+        let nomeDaCidade = prompt("Qual o nome da cidade que você mora?")
+        alert (`Estive em ${nomeDaCidade} e lembrei de você!`)
+    }
+        function somaNumeros() {
+            let valor1 = parseInt(prompt("Digite um número:"));
+            let valor2 = parseInt(prompt("Digite outro número:"));
+            let resultado = valor1 + valor2;
+            alert(`A soma de ${valor1} e ${valor2} é ${resultado}`);
+        }
+
