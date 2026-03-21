7ae195ea13ee8235f9c37bc6b9c6c2945a9388f2
bellalauletta <laulettaisabella@gmail.com>
2026-03-21 14:38:47 -0300

    função

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector("h1");
+    titulo.innerHTML = "Jogo do número secreto";
+
+    let paragrafo = document.querySelector("p");
+    paragrafo.innerHTML = "Tente advinhar o número secreto entre 1 e 10";
+
+    function verificarChute() { 
+        console.log("o botão foi clicado")
+    }
