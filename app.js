1e5122174181176e8caebddf59c77ad71ec3fd10
bellalauletta <laulettaisabella@gmail.com>
2026-03-25 12:19:04 -0300

    funções unificadas

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-let titulo = document.querySelector("h1");
-    titulo.innerHTML = "hora do desafio";
+//let titulo = document.querySelector("h1");
+    //titulo.innerHTML = "Jogo do número secreto";
 
-    function verificarBotaoChute() { 
+//let paragrafo = document.querySelector("p");
+     // paragrafo.innerHTML = "Tente advinhar o número secreto entre 1 e 10";
+    
+    function exibirTextoNaTela(tag, texto) {
+        let campo = document.querySelector(tag);
+        campo.innerHTML = texto;
+    }
+exibirTextoNaTela("h1", "Jogo do número secreto");
+exibirTextoNaTela("p", "Tente advinhar o número secreto entre 1 e 10");
+
+
+    function verificarChute() { 
         console.log("o botão foi clicado")
     }
-
-    function verificarBotaoAlerta() {
-        alert("Eu amo JS")
-    }
-    function exibirPrompt () {
-        let nomeDaCidade = prompt("Qual o nome da cidade que você mora?")
-        alert (`Estive em ${nomeDaCidade} e lembrei de você!`)
-    }
-        function somaNumeros() {
-            let valor1 = parseInt(prompt("Digite um número:"));
-            let valor2 = parseInt(prompt("Digite outro número:"));
-            let resultado = valor1 + valor2;
-            alert(`A soma de ${valor1} e ${valor2} é ${resultado}`);
-        }
-
+    
